85b4a505ebf772b5174338ecff3671a9ddb28a85
Natália Ferreira Lima <ferlima.natalia@gmail.com>
2022-04-09 23:03:09 -0300

    tentativa de testes

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -1,52 +1,59 @@
-// import * as sinon from 'sinon';
-// import * as chai from 'chai';
-// import chaiHttp = require('chai-http');
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
 
-// import { app } from '../app';
-// import Example from '../database/models/ExampleModel';
+import { app } from '../app';
+import Example from '../database/models/ExampleModel';
 
-// import { Response } from 'superagent';
-// import User from '../database/models/User';
-// import user from './mocks/user';
+import { Response } from 'superagent';
+import User from '../database/models/User';
+import user from './mocks/user';
 
-// chai.use(chaiHttp);
+chai.use(chaiHttp);
 
-// const { expect } = chai;
+const { expect } = chai;
 
-// describe('Testa a rota post /login', () => {
-//   /**
-//    * Exemplo do uso de stubs com tipos
-//    */
+describe('Testa a rota post /login e /login/validate', () => {
+  /**
+   * Exemplo do uso de stubs com tipos
+   */
 
-//   let chaiHttpResponse: Response;
+  let chaiHttpResponse: Response;
 
-//   const userMock = {
-//     user: {
-//       id: 1,
-//       username: "Admin",
-//       role: "admin",
-//       email: "admin@admin.com"
-//     },
-//     token: "123.456.789"
-//   }
+  const userMock = {
+    user: {
+      id: 1,
+      username: "Admin",
+      role: "admin",
+      email: "admin@admin.com"
+    },
+    token: "123.456.789"
+  }
 
-//   before(async () => {
-//     sinon
-//       .stub(User, "findOne")
-//       .resolves(userMock);
+  before(async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves(userMock);
+  });
+
+  after(()=>{
+    (User.findOne as sinon.SinonStub).restore();
+  })
+
+  it('rota POST /login responde com status 200', async () => {
+    chaiHttpResponse = await chai.request(app).post('/login');
+
+    expect(chaiHttpResponse.status).to.be.eq(200);
+  });
+
+  it('rota GET /login/validate responde com status 200', async () => {
+    chaiHttpResponse = await chai.request(app).get('/login/validate');
+
+    expect(chaiHttpResponse.status).to.be.eq(200);
+  });
+
+//   it('Seu sub-teste', () => {
+//     expect(false).to.be.eq(true);
 //   });
+});
 
-//   after(()=>{
-//     (User.findOne as sinon.SinonStub).restore();
-//   })
-
-//   it('responde com status 200', async () => {
-//     chaiHttpResponse = await chai.request(app).post('/login');
-
-//     expect(chaiHttpResponse.status).to.be.eq(200);
-//   });
-
-//   // it('Seu sub-teste', () => {
-//   //   expect(false).to.be.eq(true);
-//   // });
-// });
