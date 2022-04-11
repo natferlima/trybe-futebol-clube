b220bd8d278e39d4bec08270589eca7f2267bbe0
Natália Ferreira Lima <ferlima.natalia@gmail.com>
2022-04-10 23:00:23 -0300

    adiciona testes

--- app/backend/src/services/UserService.ts
+++ app/backend/src/services/UserService.ts
@@ -3,6 +3,7 @@ import User from '../database/models/User';
 export default class UserService {
   static async findByEmail(email: string) {
     const result = await User.findOne({ where: { email } });
+    console.log(result);
     return result;
   }
 }
--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -1,65 +1,75 @@
-// import * as sinon from 'sinon';
-// import * as chai from 'chai';
-// import chaiHttp = require('chai-http');
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
 
-// import { app } from '../app';
-// import Example from '../database/models/ExampleModel';
+import { app } from '../app';
+import Example from '../database/models/ExampleModel';
 
-// import { Response } from 'superagent';
-// import User from '../database/models/User';
-// import user from './mocks/user';
+import { Response } from 'superagent';
+import User from '../database/models/User';
+import user from './mocks/user';
 
-// chai.use(chaiHttp);
+chai.use(chaiHttp);
 
-// const { expect } = chai;
+const { expect } = chai;
 
-// describe('Testa a rota post /login e get /login/validate', () => {
-//   /**
-//    * Exemplo do uso de stubs com tipos
-//    */
+describe('Testa a rota post /login e get /login/validate', () => {
+  /**
+   * Exemplo do uso de stubs com tipos
+   */
 
-//   let chaiHttpResponse: Response;
+  let chaiHttpResponse: Response;
 
-//   const userMock = {
-//     user: {
-//       id: 1,
-//       username: "Admin",
-//       role: "admin",
-//       email: "admin@admin.com"
-//     },
-//     token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInBhc3N3b3JkIjoic2VjcmV0X2FkbWluIiwiaWF0IjoxNjQ5NjM1MDk4LCJleHAiOjE2NTA0OTkwOTh9.HpnalqZu1g9vkiLEUSHMgX3FUU7tIVgTI60cNTbAHtE"
-//   }
+  const findOneMock = {
+    id: 1,
+    username: 'Admin',
+    role: 'admin',
+    email: 'admin@admin.com',
+    password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW'
+  }
 
-//   before(async () => {
-//     sinon
-//       .stub(User, "findOne")
-//       .resolves(userMock);
+  const userMock = {
+    user: {
+      id: 1,
+      username: "Admin",
+      role: "admin",
+      email: "admin@admin.com"
+    },
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFkbWluLmNvbSIsInBhc3N3b3JkIjoic2VjcmV0X2FkbWluIiwiaWF0IjoxNjQ5NjM1MDk4LCJleHAiOjE2NTA0OTkwOTh9.HpnalqZu1g9vkiLEUSHMgX3FUU7tIVgTI60cNTbAHtE"
+  }
+
+  before(async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves(findOneMock as any);
+  });
+
+  after(()=>{
+    (User.findOne as sinon.SinonStub).restore();
+  })
+
+  it('rota POST /login responde com status 200', async () => {
+    chaiHttpResponse = await chai.request(app).post('/login')
+    .send({
+      email: "admin@admin.com",
+      password: "secret_admin"
+    });
+
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).to.haveOwnProperty('token');
+    expect(chaiHttpResponse.body.user).to.be.deep.equals(userMock.user);
+  });
+
+  it('rota GET /login/validate responde com status 200', async () => {
+    chaiHttpResponse = await chai.request(app).get('/login/validate')
+    .set('Authorization', userMock.token);
+
+    expect(chaiHttpResponse.status).to.be.equal(200);
+    expect(chaiHttpResponse.body).to.be.deep.equals('admin');
+  });
+
+//   it('Seu sub-teste', () => {
+//     expect(false).to.be.eq(true);
 //   });
-
-//   after(()=>{
-//     (User.findOne as sinon.SinonStub).restore();
-//   })
-
-//   it('rota POST /login responde com status 200', async () => {
-//     chaiHttpResponse = await chai.request(app).post('/login')
-//     .send({
-//       email: "admin@admin.com",
-//       password: "secret_admin"
-//     });
-
-//     expect(chaiHttpResponse.status).to.be.eq(200);
-//     expect(chaiHttpResponse.body).to.haveOwnProperty('token');
-//     expect(chaiHttpResponse.body.user).to.be.deep.equals(userMock.user);
-//   });
-
-//   it('rota GET /login/validate responde com status 200', async () => {
-//     chaiHttpResponse = await chai.request(app).get('/login/validate');
-
-//     expect(chaiHttpResponse.status).to.be.eq(200);
-//   });
-
-// //   it('Seu sub-teste', () => {
-// //     expect(false).to.be.eq(true);
-// //   });
-// });
+});
 
